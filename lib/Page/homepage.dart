2baf9f0911423413849ad790920549c984d2932c
Syed Osama Ahmed <batch17.026@gmail.com>
2023-03-12 10:58:24 +0500

    remove onchange from textfield

--- lib/Page/homepage.dart
+++ lib/Page/homepage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:developer' as logs show log;
+
 import 'package:hive_example/boxes.dart';
 import 'package:hive_example/models/books.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -86,85 +86,87 @@ class _HomePageState extends State<HomePage> {
       body: SizedBox(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        child: Column(children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: const InputDecoration(hintText: 'Enter Book Name'),
-              controller: name,
-              onChanged: (value) {
-                name.text = value;
-              },
+        child: SingleChildScrollView(
+          child: Column(children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: const InputDecoration(hintText: 'Enter Book Name'),
+                controller: name,
+                // onChanged: (value) {
+                //   name.text = value;
+                // },
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 12,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              onChanged: (value) {
-                id.text = value;
-                logs.log(id.text);
-              },
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(hintText: 'Enter Book ID'),
-              controller: id,
+            const SizedBox(
+              height: 12,
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  Books book = Books(name.text, int.parse(id.text));
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                // onChanged: (value) {
+                //   id.text = value;
+                //   logs.log(id.text);
+                // },
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(hintText: 'Enter Book ID'),
+                controller: id,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Books book = Books(name.text, int.parse(id.text));
 
-                  box.add(book);
-                  book.save();
-                  id.clear();
-                  name.clear();
-                },
-                child: const Text('Add'),
-              ),
-              const SizedBox(
-                width: 12,
-                height: 12,
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 12,
-          ),
-          ValueListenableBuilder(
-            valueListenable: Boxes.getBooks().listenable(),
-            builder: (context, box, child) {
-              var data = box.values.toList().cast<Books>();
-              return SizedBox(
-                height: MediaQuery.of(context).size.height * 0.30,
-                child: ListView.builder(
-                  itemCount: box.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(data[index].name),
-                      contentPadding: const EdgeInsets.all(16.0),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          InkWell(
-                              onTap: () => delete(data[index]),
-                              child: const Icon(Icons.delete)),
-                          InkWell(
-                              onTap: () => update(data[index]),
-                              child: const Icon(Icons.edit)),
-                        ],
-                      ),
-                    );
+                    box.add(book);
+                    book.save();
+                    id.clear();
+                    name.clear();
                   },
+                  child: const Text('Add'),
                 ),
-              );
-            },
-          ),
-        ]),
+                const SizedBox(
+                  width: 12,
+                  height: 12,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            ValueListenableBuilder(
+              valueListenable: Boxes.getBooks().listenable(),
+              builder: (context, box, child) {
+                var data = box.values.toList().cast<Books>();
+                return SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.30,
+                  child: ListView.builder(
+                    itemCount: box.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(data[index].name),
+                        contentPadding: const EdgeInsets.all(16.0),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            InkWell(
+                                onTap: () => delete(data[index]),
+                                child: const Icon(Icons.delete)),
+                            InkWell(
+                                onTap: () => update(data[index]),
+                                child: const Icon(Icons.edit)),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+          ]),
+        ),
       ),
     );
   }
